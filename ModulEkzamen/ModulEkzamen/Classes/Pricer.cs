3808dbf943bf3dff9754be2aa157cd233b6e72b2
МузуроваСС@ngknn.local <@ngknn.local>
2021-09-14 15:38:16 +0300

    Добавьте файлы проекта.

---
+++ ModulEkzamen/ModulEkzamen/Classes/Pricer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModulEkzamen.Classes
+{
+    class Pricer
+    {
+        public static string Price1(int x, int c)
+        {
+            int b = x + c;
+            return Convert.ToString(b);
+        }
+        public static string Price2(int x, int c)
+        {
+            int b = x + c;
+            return Convert.ToString(b);
+        }
+        public static int Discount(int x, int v)
+        {
+            int j = 0;
+            int b = v%500;
+            if(x> 3)
+            {
+               j = 5;
+            }
+            else if (x > 5)
+            {
+                j = 10;
+            }
+            else if (x > 10)
+            {
+                j = 15;
+            }
+            return b + j;
+        }
+    }
+}
